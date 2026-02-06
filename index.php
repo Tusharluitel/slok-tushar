e457e7bcfff19ea8804b123cbb936b3336e71208
slok14 <gunnerslok@gmail.com>
2026-02-06 11:58:18 +0545

    fix merge file

--- index.php
+++ index.php
@@ -5,3 +5,11 @@ echo "Hello, World!";
 ?>
 <h1>Welcome to my website</h1>
 <p>This is a sample webpage.</p>
+<table>
+  <tr>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Email</th>
+    <th>City</th>
+  </tr>
+</table>
